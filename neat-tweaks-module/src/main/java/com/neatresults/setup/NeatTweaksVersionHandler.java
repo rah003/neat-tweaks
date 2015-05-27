732bda2bf5b7ae9e3ad4d38024daeb51320fc9cc
Jan Haderka <jan.haderka@magnolia-cms.com>
2015-05-27 15:00:03 +0200

    rebootstrap the app and remove duplicated config

--- neat-tweaks-module/src/main/java/com/neatresults/setup/NeatTweaksVersionHandler.java
+++ neat-tweaks-module/src/main/java/com/neatresults/setup/NeatTweaksVersionHandler.java
@@ -1,7 +1,10 @@
 package com.neatresults.setup;
 
+import info.magnolia.jcr.util.NodeTypes;
 import info.magnolia.module.DefaultModuleVersionHandler;
 import info.magnolia.module.InstallContext;
+import info.magnolia.module.delta.CreateNodeTask;
+import info.magnolia.module.delta.MoveNodeTask;
 import info.magnolia.module.delta.Task;
 import info.magnolia.module.resources.setup.InstallTextResourceTask;
 
@@ -19,6 +22,8 @@ public class NeatTweaksVersionHandler extends DefaultModuleVersionHandler {
     protected List<Task> getExtraInstallTasks(InstallContext installContext) {
         List<Task> tasks = new ArrayList<Task>(super.getExtraInstallTasks(installContext));
         tasks.add(new InstallTextResourceTask("/admincentral/custom_theme.css"));
+        tasks.add(new CreateNodeTask("", "/modules/ui-admincentral/config/appLauncherLayout/groups/manage/apps", "neatconfiguration", NodeTypes.ContentNode.NAME));
+        tasks.add(new MoveNodeTask("", "/modules/ui-admincentral/config/appLauncherLayout/groups/manage/apps/configuration", "/modules/ui-admincentral/config/appLauncherLayout/groups/tools/apps/configuration", false));
         return tasks;
     }
 }
