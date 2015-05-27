500d338c98794e18d6470fbf4c7bcd295125ac07
Jan Haderka <jan.haderka@magnolia-cms.com>
2015-05-27 12:58:26 +0200

    Customized theme for magnolia to make editing easier

--- neat-tweaks-module/src/main/java/com/neatresults/setup/NeatTweaksVersionHandler.java
+++ neat-tweaks-module/src/main/java/com/neatresults/setup/NeatTweaksVersionHandler.java
@@ -1,6 +1,12 @@
 package com.neatresults.setup;
 
 import info.magnolia.module.DefaultModuleVersionHandler;
+import info.magnolia.module.InstallContext;
+import info.magnolia.module.delta.Task;
+import info.magnolia.module.resources.setup.InstallTextResourceTask;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class is optional and lets you manager the versions of your module,
@@ -9,4 +15,10 @@ import info.magnolia.module.DefaultModuleVersionHandler;
  */
 public class NeatTweaksVersionHandler extends DefaultModuleVersionHandler {
 
+    @Override
+    protected List<Task> getExtraInstallTasks(InstallContext installContext) {
+        List<Task> tasks = new ArrayList<Task>(super.getExtraInstallTasks(installContext));
+        tasks.add(new InstallTextResourceTask("/admincentral/custom_theme.css"));
+        return tasks;
+    }
 }
