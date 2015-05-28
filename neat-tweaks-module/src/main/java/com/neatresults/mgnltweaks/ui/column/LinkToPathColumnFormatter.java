8d195f67222eab18560a0d5a023f66f0134cf6ce
Jan Haderka <jan.haderka@magnolia-cms.com>
2015-05-28 17:10:14 +0200

    semi-fixed issue with links not jumping to correct location in config app, added links for ids

--- neat-tweaks-module/src/main/java/com/neatresults/mgnltweaks/ui/column/LinkToPathColumnFormatter.java
+++ neat-tweaks-module/src/main/java/com/neatresults/mgnltweaks/ui/column/LinkToPathColumnFormatter.java
@@ -2,10 +2,17 @@ package com.neatresults.mgnltweaks.ui.column;
 
 import info.magnolia.context.MgnlContext;
 import info.magnolia.event.EventBus;
+import info.magnolia.ui.api.app.SubAppContext;
+import info.magnolia.ui.api.app.SubAppEventBus;
 import info.magnolia.ui.api.event.AdmincentralEventBus;
 import info.magnolia.ui.api.location.DefaultLocation;
 import info.magnolia.ui.api.location.Location;
+import info.magnolia.ui.api.location.LocationChangedEvent;
 import info.magnolia.ui.api.location.LocationController;
+import info.magnolia.ui.contentapp.browser.BrowserLocation;
+import info.magnolia.ui.contentapp.browser.ConfiguredBrowserSubAppDescriptor;
+import info.magnolia.ui.vaadin.integration.contentconnector.ConfiguredJcrContentConnectorDefinition;
+import info.magnolia.ui.vaadin.integration.contentconnector.ContentConnectorDefinition;
 import info.magnolia.ui.vaadin.integration.jcr.JcrNodeItemId;
 import info.magnolia.ui.vaadin.integration.jcr.JcrPropertyAdapter;
 import info.magnolia.ui.workbench.column.AbstractColumnFormatter;
@@ -15,6 +22,7 @@ import info.magnolia.ui.workbench.column.definition.MetaDataColumnDefinition;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
@@ -24,6 +32,7 @@ import org.slf4j.LoggerFactory;
 
 import com.vaadin.data.Item;
 import com.vaadin.data.Property;
+import com.vaadin.server.Page;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
@@ -37,13 +46,17 @@ public class LinkToPathColumnFormatter extends AbstractColumnFormatter<ColumnDef
 
     private static final Logger log = LoggerFactory.getLogger(LinkToPathColumnFormatter.class);
     private LocationController locationController;
+    private EventBus adminEventBus;
     private EventBus eventBus;
+    private SubAppContext subAppContext;
 
     @Inject
-    public LinkToPathColumnFormatter(Definition definition, LocationController locationController, @Named(AdmincentralEventBus.NAME) final EventBus eventBus) {
+    public LinkToPathColumnFormatter(Definition definition, LocationController locationController, @Named(AdmincentralEventBus.NAME) final EventBus adminEventBus, @Named(SubAppEventBus.NAME) final EventBus eventBus, SubAppContext subAppContext) {
         super(definition);
         this.locationController = locationController;
+        this.adminEventBus = adminEventBus;
         this.eventBus = eventBus;
+        this.subAppContext = subAppContext;
     }
 
     @Override
@@ -64,11 +77,16 @@ public class LinkToPathColumnFormatter extends AbstractColumnFormatter<ColumnDef
                     log.debug(e.getMessage(), e);
                 }
                 if ("templateScript".equals(propName) && path.startsWith("/") && path.endsWith(".ftl")) {
-                    return createLinkButton("templates", path);
+                    return createLinkButton(path, "templates", StringUtils.substringBefore(path, ".ftl"));
                 } else if ("dialog".equals(propName) || "dialogName".equals(propName)) {
                     String title = path;
                     String[] parts = path.split(":");
                     path = "/modules/" + parts[0] + "/dialogs/" + parts[1];
+                    return createLinkButton(title, "config", path);
+                } else if ("id".equals(propName)) {
+                    String title = path;
+                    String[] parts = path.split(":");
+                    path = "/modules/" + parts[0] + "/templates/" + parts[1];
                     return createLinkButton(title, "config", path);
                 } else if ("extends".equals(propName)) {
                     String title = path;
@@ -90,32 +108,36 @@ public class LinkToPathColumnFormatter extends AbstractColumnFormatter<ColumnDef
         return null;
     }
 
-    private Object createLinkButton(String workspace, String path) {
-        return createLinkButton(path, workspace, path);
-    }
     private Object createLinkButton(String title, String workspace, String path) {
         try {
             Session session = MgnlContext.getJCRSession(workspace);
             String appName = null;
             if ("templates".equals(workspace) && session.nodeExists(StringUtils.substringBeforeLast(path, "."))) {
                 appName = "inplace-templating";
-                return createButton(appName, "detail", path, new JcrNodeItemId(session.getNode(StringUtils.substringBeforeLast(path, ".")).getIdentifier(), workspace));
+                return createButton(title, appName, "detail", path, new JcrNodeItemId(session.getNode(StringUtils.substringBeforeLast(path, ".")).getIdentifier(), workspace), "");
             } else if ("config".equals(workspace) && session.nodeExists(path)) {
-                appName = "configuration";
-                return createButton(title, appName, "browser", path, new JcrNodeItemId(session.getNode(path).getIdentifier(), workspace));
+                String rootPath = "";
+                if (subAppContext.getSubAppDescriptor() instanceof ConfiguredBrowserSubAppDescriptor) {
+                    ContentConnectorDefinition connector = ((ConfiguredBrowserSubAppDescriptor) subAppContext.getSubAppDescriptor()).getContentConnector();
+                    if (connector instanceof ConfiguredJcrContentConnectorDefinition) {
+                        rootPath = ((ConfiguredJcrContentConnectorDefinition) connector).getRootPath();
+                    }
+                }
+
+                appName = subAppContext.getAppContext().getName();
+                Node node = session.getNode(path);
+                // remove relative segments from path
+                path = node.getPath();
+                return createButton(title, appName, "browser", path, new JcrNodeItemId(node.getIdentifier(), workspace), rootPath);
             }
         } catch (RepositoryException e) {
             log.debug(e.getMessage(), e);
         }
-        // need to return title (original value) as path might have been trasnformed already
+        // need to return title (original value) as path might have been transformed already
         return title;
     }
 
-    private Button createButton(final String appName, final String subAppName, final String path, Object itemId) {
-        return createButton(path, appName, subAppName, path, itemId);
-    }
-
-    private Button createButton(final String title, final String appName, final String subAppName, final String path, final Object itemId) {
+    private Button createButton(final String title, final String appName, final String subAppName, final String path, final Object itemId, final String rootPath) {
         Button selectButton = new NativeButton();
         selectButton.addStyleName("neatmagnoliabutton");
         selectButton.setCaption(title);
@@ -123,9 +145,27 @@ public class LinkToPathColumnFormatter extends AbstractColumnFormatter<ColumnDef
 
             @Override
             public void buttonClick(ClickEvent event) {
-                Location location = null;
-                location = new DefaultLocation("app", appName, subAppName, path);
-                locationController.goTo(location);
+                String workPath = path;
+                if (StringUtils.isNotBlank(rootPath) && !"/".equals(rootPath)) {
+                    workPath = StringUtils.removeStart(workPath, rootPath);
+                }
+                if ("browser".equals(subAppName)) {
+                    Location location = new BrowserLocation(appName, subAppName, workPath + ":treeview:");
+                    System.out.println("new loc:" + location);
+                    adminEventBus.fireEvent(new LocationChangedEvent(location));
+                    // FYI: sucks on so many levels, but for some reason vaadin/mangolia will not scroll properly to new location within same browser w/o slight delay introduced by popup window below
+                    String message = "window.alert('stand by for beam up to new location')";
+                    try {
+                        Thread.currentThread().sleep(500);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                    Page.getCurrent().getJavaScript().execute(message);
+
+                } else {
+                    Location location = new DefaultLocation("app", appName, subAppName, workPath);
+                    adminEventBus.fireEvent(new LocationChangedEvent(location));
+                }
             }
         });
         return selectButton;
