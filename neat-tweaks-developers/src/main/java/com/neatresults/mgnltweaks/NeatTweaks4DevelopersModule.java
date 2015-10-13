9efd0a587a5370e1280fac0cdccd7add2472568c
Jan Haderka <jan.haderka@magnolia-cms.com>
2015-10-13 08:11:25 +0200

    dependencies and module class

---
+++ neat-tweaks-developers/src/main/java/com/neatresults/mgnltweaks/NeatTweaks4DevelopersModule.java
@@ -0,0 +1,18 @@
+package com.neatresults.mgnltweaks;
+
+import info.magnolia.module.ModuleLifecycle;
+import info.magnolia.module.ModuleLifecycleContext;
+
+public class NeatTweaks4DevelopersModule implements ModuleLifecycle {
+
+    @Override
+    public void start(ModuleLifecycleContext ctx) {
+
+    }
+
+    @Override
+    public void stop(ModuleLifecycleContext ctx) {
+
+    }
+
+}
