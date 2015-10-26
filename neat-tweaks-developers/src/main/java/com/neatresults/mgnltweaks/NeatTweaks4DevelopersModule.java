6bac1996475bde498f660c33d207d6ef71abe0d0
Jan Haderka <jan.haderka@magnolia-cms.com>
2015-10-26 23:48:03 +0100

    Support for preferredModules (sorted out of 'normal' order on top of module list. Added support for sorting subnodes of modules

--- neat-tweaks-developers/src/main/java/com/neatresults/mgnltweaks/NeatTweaks4DevelopersModule.java
+++ neat-tweaks-developers/src/main/java/com/neatresults/mgnltweaks/NeatTweaks4DevelopersModule.java
@@ -1,3 +1,28 @@
+/**
+ *
+ * Copyright 2015 by Jan Haderka <jan.haderka@neatresults.com>
+ *
+ * This file is part of neat-tweaks module.
+ *
+ * Neat-tweaks is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Neat-tweaks is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with neat-tweaks.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license GPL-3.0 <http://www.gnu.org/licenses/gpl.txt>
+ *
+ * Should you require distribution under alternative license in order to
+ * use neat-tweaks commercially, please contact owner at the address above.
+ *
+ */
 package com.neatresults.mgnltweaks;
 
 import info.magnolia.context.MgnlContext;
@@ -8,6 +33,7 @@ import info.magnolia.module.ModuleLifecycle;
 import info.magnolia.module.ModuleLifecycleContext;
 import info.magnolia.repository.RepositoryConstants;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -24,6 +50,7 @@ import com.google.common.collect.Ordering;
 public class NeatTweaks4DevelopersModule implements ModuleLifecycle {
 
     private boolean showSubtreeOnlyInHelper = true;
+    private List<ModuleName> preferredModules = new ArrayList<ModuleName>();
 
     @Override
     public void start(ModuleLifecycleContext ctx) {
@@ -37,40 +64,61 @@ public class NeatTweaks4DevelopersModule implements ModuleLifecycle {
         try {
             Session configSession = MgnlContext.getJCRSession(RepositoryConstants.CONFIG);
             Node modules = configSession.getNode("/modules");
-            // get all child nodes
-            NodeIterator iter = new FilteringNodeIterator(modules.getNodes(), new AbstractPredicate<Node>() {
-
-                @Override
-                public boolean evaluateTyped(Node t) {
-                    try {
-                        String name = t.getName();
-                        return !name.startsWith("jcr:") && !name.startsWith("rep:");
-                    } catch (RepositoryException e) {
-                        throw new RuntimeException(e);
+            List<Node> orderedModules = orderChildNodes(modules);
+            // break out order for preferred modules and put those on top of the list
+            for (ModuleName name : preferredModules) {
+                for (Node n : orderedModules) {
+                    if (n.getName().equals(name.getName())) {
+                        orderedModules.remove(n);
+                        orderedModules.add(0, n);
+                        break;
                     }
                 }
-            });
-            Comparator<Node> comparator = new Comparator<Node>() {
-                @Override
-                public int compare(Node o1, Node o2) {
-                    try {
-                        return o1.getName().compareTo(o2.getName());
-                    } catch (RepositoryException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            };
-            // order them
-            List<Node> ordered = Ordering.from(comparator).sortedCopy(NodeUtil.asIterable(iter));
+            }
             // by order, place all at the end of the list
-            for (Node n : ordered) {
+            // this is for modules
+            for (Node n : orderedModules) {
                 modules.orderBefore(n.getName(), null);
+                List<Node> ordered = orderChildNodes(n);
+                // by order, place all at the end of the list
+                // this is for apps, templates, and other nodes under each module
+                for (Node o : ordered) {
+                    n.orderBefore(o.getName(), null);
+                }
+
             }
             // save
             configSession.save();
         } catch (RepositoryException e) {
 
         }
+    }
+
+    private List<Node> orderChildNodes(Node parent) throws RepositoryException {
+        NodeIterator iter = new FilteringNodeIterator(parent.getNodes(), new AbstractPredicate<Node>() {
+
+            @Override
+            public boolean evaluateTyped(Node t) {
+                try {
+                    String name = t.getName();
+                    return !name.startsWith("jcr:") && !name.startsWith("rep:");
+                } catch (RepositoryException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Comparator<Node> comparator = new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                try {
+                    return o1.getName().compareTo(o2.getName());
+                } catch (RepositoryException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        // order them
+        return Ordering.from(comparator).sortedCopy(NodeUtil.asIterable(iter));
     }
 
     @Override
@@ -84,6 +132,14 @@ public class NeatTweaks4DevelopersModule implements ModuleLifecycle {
 
     public void setShowSubtreeOnlyInHelper(boolean showSubtreeOnlyInHelper) {
         this.showSubtreeOnlyInHelper = showSubtreeOnlyInHelper;
+    }
+
+    public List<ModuleName> getPreferredModules() {
+        return preferredModules;
+    }
+
+    public void setPreferredModules(List<ModuleName> preferredModules) {
+        this.preferredModules = preferredModules;
     }
 
 }
