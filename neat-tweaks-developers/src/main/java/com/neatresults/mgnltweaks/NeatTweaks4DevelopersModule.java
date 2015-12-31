b76b04f4270e0bab1e9f4471ac6a0dc959c22158
Jan Haderka <jan.haderka@magnolia-cms.com>
2015-12-31 11:49:01 +0100

    Support preferred modules when reordering. Do not reorder unless necessary to minimize impact of #6

--- neat-tweaks-developers/src/main/java/com/neatresults/mgnltweaks/NeatTweaks4DevelopersModule.java
+++ neat-tweaks-developers/src/main/java/com/neatresults/mgnltweaks/NeatTweaks4DevelopersModule.java
@@ -1,4 +1,43 @@
+/**
+ *
+ * Copyright 2015 by Jan Haderka <jan.haderka@neatresults.com>
+ *
+ * This file is part of neat-tweaks module.
+ *
+ * Neat-tweaks is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Neat-tweaks is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with neat-tweaks.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license GPL-3.0 <http://www.gnu.org/licenses/gpl.txt>
+ *
+ * Should you require distribution under alternative license in order to
+ * use neat-tweaks commercially, please contact owner at the address above.
+ *
+ */
 package com.neatresults.mgnltweaks;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Ordering;
 
 import info.magnolia.context.MgnlContext;
 import info.magnolia.jcr.iterator.FilteringNodeIterator;
@@ -8,26 +47,23 @@ import info.magnolia.module.ModuleLifecycle;
 import info.magnolia.module.ModuleLifecycleContext;
 import info.magnolia.repository.RepositoryConstants;
 
-import java.util.Comparator;
-import java.util.List;
-
-import javax.jcr.Node;
-import javax.jcr.NodeIterator;
-import javax.jcr.RepositoryException;
-import javax.jcr.Session;
-
-import com.google.common.collect.Ordering;
-
 /**
  * NeatTweaks4DevelopersModule class.
  */
 public class NeatTweaks4DevelopersModule implements ModuleLifecycle {
 
+    private static final Logger log = LoggerFactory.getLogger(NeatTweaks4DevelopersModule.class);
+
+    private boolean forceModuleOrder = true;
     private boolean showSubtreeOnlyInHelper = true;
+    private List<ModuleName> preferredModules = new ArrayList<ModuleName>();
 
     @Override
     public void start(ModuleLifecycleContext ctx) {
-        orderModules();
+        if (forceModuleOrder) {
+            log.error("Will commence module reordering");
+            orderModules();
+        }
     }
 
     /**
@@ -37,40 +73,95 @@ public class NeatTweaks4DevelopersModule implements ModuleLifecycle {
         try {
             Session configSession = MgnlContext.getJCRSession(RepositoryConstants.CONFIG);
             Node modules = configSession.getNode("/modules");
-            // get all child nodes
-            NodeIterator iter = new FilteringNodeIterator(modules.getNodes(), new AbstractPredicate<Node>() {
+            List<Node> natural = getChildList(modules);
+            List<Node> orderedModules = orderChildNodes(natural);
+            // break out order for preferred modules and put those on top of the list
+            for (ModuleName name : preferredModules) {
+                for (Node n : orderedModules) {
+                    if (n.getName().equals(name.getName())) {
+                        orderedModules.remove(n);
+                        orderedModules.add(0, n);
+                        break;
+                    }
+                }
+            }
 
-                @Override
-                public boolean evaluateTyped(Node t) {
-                    try {
-                        String name = t.getName();
-                        return !name.startsWith("jcr:") && !name.startsWith("rep:");
-                    } catch (RepositoryException e) {
-                        throw new RuntimeException(e);
-                    }
+            boolean needResort = false;
+            for (int idx = 0; idx < natural.size(); idx++) {
+                if (natural.get(idx) != orderedModules.get(idx)) {
+                    // need to get sorted
+                    needResort = true;
+                    break;
                 }
-            });
-            Comparator<Node> comparator = new Comparator<Node>() {
-                @Override
-                public int compare(Node o1, Node o2) {
-                    try {
-                        return o1.getName().compareTo(o2.getName());
-                    } catch (RepositoryException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            };
-            // order them
-            List<Node> ordered = Ordering.from(comparator).sortedCopy(NodeUtil.asIterable(iter));
+            }
+
+            log.info("Need to reorder modules? " + needResort);
+
             // by order, place all at the end of the list
-            for (Node n : ordered) {
-                modules.orderBefore(n.getName(), null);
+            // this is for modules
+            for (Node n : orderedModules) {
+                if (needResort) {
+                    modules.orderBefore(n.getName(), null);
+                }
+                try {
+                    List<Node> naturalChildren = getChildList(n);
+                    List<Node> ordered = orderChildNodes(naturalChildren);
+                    boolean needChildResort = false;
+                    for (int idx = 0; idx < naturalChildren.size(); idx++) {
+                        if (naturalChildren.get(idx) != ordered.get(idx)) {
+                            // need to get sorted
+                            needChildResort = true;
+                            break;
+                        }
+                    }
+                    if (needChildResort) {
+                        // by order, place all at the end of the list
+                        // this is for apps, templates, and other nodes under each module
+                        for (Node o : ordered) {
+                            n.orderBefore(o.getName(), null);
+                        }
+                    }
+                } catch (RepositoryException e) {
+                    log.debug(e.getMessage(), e);
+                }
             }
             // save
             configSession.save();
         } catch (RepositoryException e) {
-
+            log.debug(e.getMessage(), e);
         }
+    }
+
+    private List<Node> getChildList(Node parent) throws RepositoryException {
+        NodeIterator iter = new FilteringNodeIterator(parent.getNodes(), new AbstractPredicate<Node>() {
+            @Override
+            public boolean evaluateTyped(Node t) {
+                try {
+                    String name = t.getName();
+                    return !name.startsWith("jcr:") && !name.startsWith("rep:");
+                } catch (RepositoryException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        return new ArrayList<Node>(NodeUtil.getCollectionFromNodeIterator(iter));
+    }
+
+    private List<Node> orderChildNodes(List<Node> natural) throws RepositoryException {
+        Comparator<Node> comparator = new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                try {
+                    return o1.getName().compareTo(o2.getName());
+                } catch (RepositoryException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        // order them
+        return Ordering.from(comparator).sortedCopy(natural);
+
     }
 
     @Override
@@ -84,6 +175,22 @@ public class NeatTweaks4DevelopersModule implements ModuleLifecycle {
 
     public void setShowSubtreeOnlyInHelper(boolean showSubtreeOnlyInHelper) {
         this.showSubtreeOnlyInHelper = showSubtreeOnlyInHelper;
+    }
+
+    public List<ModuleName> getPreferredModules() {
+        return preferredModules;
+    }
+
+    public void setPreferredModules(List<ModuleName> preferredModules) {
+        this.preferredModules = preferredModules;
+    }
+
+    public boolean isForceModuleOrder() {
+        return forceModuleOrder;
+    }
+
+    public void setForceModuleOrder(boolean forceModuleOrder) {
+        this.forceModuleOrder = forceModuleOrder;
     }
 
 }
